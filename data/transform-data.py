22ef944aeafac896ecb7ee7993e3d9efa1eae683
Alan Lau <melaus@me.com>
2016-02-14 16:40:20 +0000

    fixed reconstruction of pixels after prediction

--- data/transform-data.py
+++ data/transform-data.py
@@ -20,47 +20,21 @@ def initialise():
 
 
 """
-find the depths of the required areas with respect to 
-the size of an image and the required dimension
-
-input:
-    x   : width
-    y   : height
-    dim : dimension
-
-return: 
-    x_max : accessible pixels on the x-axis
-    y_max : accessible pixels on the y-axis
-    areas : the points
+generic way to find depths given boundaries and dimension
 """
-def gen_area_depths(x, y, dim):
-    # check what is the maximum area that the dimension can cover
-    excess_x = x % dim
-    excess_y = y % dim
+def get_depths(x_min, x_max, y_min, y_max, dim):
     
-    x_min = int(math.ceil(excess_x/2-1)) if excess_x > 0 else 0 
-    x_max = x - int(math.floor(excess_x/2)) if excess_x > 0 else x 
-
-    y_min = int(math.ceil(excess_y/2-1)) if excess_y > 0 else 0 
-    y_max = y - int(math.floor(excess_y/2)) if excess_y > 0 else y
-
-    x_lower = x_min
-    x_upper = x_min + dim
-
-    y_lower = y_min
-    y_upper = y_min + dim
-
-    print 'x_min:', x_min, ' x_max:', x_max, ' y_min:', y_min, ' y_max:', y_max
-    print 'x_lower:', x_lower, 'x_upper:', x_upper, 'y_lower:', y_lower, 'y_upper:', y_upper
-    print dim
+    x_lower, x_upper = x_min, x_min+dim
+    y_lower, y_upper = y_min, y_min+dim
 
     tmp = []
     output = []
 
-    for y_groups in range(0, y_max/dim):
-	for x_groups in range(0, x_max/dim):
-            #tmp = [(x, y) for y in range(y_lower, y_upper) for x in range(x_lower, x_upper)]
-            tmp = [img_depth[x][y] for y in range(y_lower, y_upper) for x in range(x_lower, x_upper)]
+    for y_groups in range(0, int(math.floor(y_max/dim))):
+	for x_groups in range(0, int(math.floor(x_max/dim))):
+            # get depths for these points
+            tmp = [img_depths[x][y] for y in range(y_lower, y_upper) for x in range(x_lower, x_upper)]
+            #tmp = [(x,y) for y in range(y_lower, y_upper) for x in range(x_lower, x_upper)]
 	    
 	    # increase the y bounds to process the next group
 	    x_lower += dim
@@ -75,13 +49,41 @@ def gen_area_depths(x, y, dim):
 	# increase y bounds to process the next group
 	y_lower += dim
 	y_upper += dim
+        
+    return output
+
+
+"""
+find the depths of the required areas with respect to 
+the size of an image and the required dimension
+
+uses get_depths
+
+input:
+    x   : width
+    y   : height
+    dim : dimension
+
+return: list of depths 
+"""
+def gen_area_depths(x, y, dim):
+    # check what is the maximum area that the dimension can cover
+    excess_x = x % dim
+    excess_y = y % dim
     
-    return output 
+    # set the min and max value
+    x_min = int(math.ceil(excess_x/2-1)) if excess_x > 0 else 0 
+    x_max = x - int(math.floor(excess_x/2)) if excess_x > 0 else x 
+
+    y_min = int(math.ceil(excess_y/2-1)) if excess_y > 0 else 0 
+    y_max = y - int(math.floor(excess_y/2)) if excess_y > 0 else y
+
+    return get_depths(x_min, x_max, y_min, y_max, dim)
 
 """
 get areas of interest
 """
-def get_object_points(label_data, lbl):
+def get_object_depths(label_data, lbl, dim):
     # find out chair areas
     coordinates = zip(*np.where(label_data == lbl))
     areas       = []
@@ -89,24 +91,17 @@ def get_object_points(label_data, lbl):
     # find the area of which we use to normalise the middle point 
     for pt in coordinates: 
         # get the range of the area concerned
-        x_min = pt[0]-7 if pt[0]-7 >= 0 else 0
-        x_max = pt[0]+8 if pt[0]+8 <= 640 else 640
+        x_min = pt[0]-int(math.floor(dim/2)) if pt[0]-int(math.floor(dim/2)) >= 0 else 0
+        x_max = pt[0]+int(math.ceil(dim/2))  if pt[0]+int(math.ceil(dim/2)) < 640 else 479 
 
-        y_min = pt[1]-7 if pt[1]-7 >= 0 else 0
-        y_max = pt[1]+8 if pt[1]+8 <= 480 else 480 
+        y_min = pt[1]-int(math.floor(dim/2)) if pt[1]-int(math.floor(dim/2)) >= 0 else 0
+        y_max = pt[1]+int(math.ceil(dim/2))  if pt[1]+int(math.ceil(dim/2)) < 480 else 479 
         
-        if (x_max-x_min == 15 & y_max-y_min == 15):
-            areas.append(zip(range(x_min, x_max)*15, sorted(range(y_min, y_max)*15)))
+        if (x_max-x_min == dim-(dim%2) & y_max-y_min == dim-(dim%2)):
+            # append depths to output list
+            areas.append([img_depths[x][y] for y in range(y_min, y_max+1) for x in range(x_min, x_max+1)])
     
     return areas 
-
-
-"""
-get the actual depth values for the points concerned
-"""
-def get_depth_values(areas):
-    # construct a list in the form of areas produced by object_area()
-    return [[img_depth[pt[0]][pt[1]] for pt in area] for area in areas]
 
 
 """
@@ -120,19 +115,17 @@ def mean_depth(areas):
 normalise depth by deducting the mean value 
 from each depth value in the area concerned
 """
-def get_normalised_depth(areas, means):
+def get_normalised_depths(areas, means):
     return [[j - b for j in a] for a, b in zip(areas,means)]
 
 
 """
-store features and target dicitonary to a file for reuse
+create feature-target dicitonary
 """
-#def store_output_dict(features, targets):
-    #output = {'features': features, 'targets': targets}
-    #pickle.dump(output, open('py-data/features_targets.p', 'wb'))
-    #print 'done'
+def create_ft_dict(features, targets):
+    return {'features': features, 'targets': targets}
 
-def store_output(data, filename):
+def store_output(data, filename, path=''):
     pickle.dump(data, open(path+filename, 'wb'))
     print 'done'
 
@@ -140,57 +133,70 @@ def store_output(data, filename):
 testing point
 """
 if __name__ == '__main__':
-    img_image, img_depth, img_label = initialise()
+    img_images, img_depths, img_labels = initialise()
 
-    #print list(set(label.flatten().tolist()))
-
-    ##plt.imshow(image[:][:][:][0])
-    ##plt.show()
-
-    ## areas that requires to be examined as features
-    #bk_pts        = get_object_points(label, 0)
-    #bk_dps        = get_depth_values(bk_pts)
+    ## background 
+    #bk_dps        = get_object_depths(img_labels, 0, 15)
     #bk_mean       = mean_depth(bk_dps)
-    #bk_normalised = get_normalised_depth(bk_dps, bk_mean)
-    #print 'background sorted, length', len(bk_mean)
+    #bk_normalised = get_normalised_depths(bk_dps, bk_mean)
+    #print 'background sorted'
+    #print ' - mean length:', len(bk_mean)
+    #print ' - dps length: ', len(bk_dps)
+    #print ' - dps set len:', set([len(i) for i in bk_dps])
+    #print ''
 
-    #ceiling_pts        = get_object_points(label, 4)
-    #ceiling_dps        = get_depth_values(ceiling_pts)
+    ## ceiling
+    #ceiling_dps        = get_object_depths(img_labels, 4, 15)
     #ceiling_mean       = mean_depth(ceiling_dps)
-    #ceiling_normalised = get_normalised_depth(ceiling_dps, ceiling_mean)
-    #print 'ceiling sorted, length', len(ceiling_mean)
+    #ceiling_normalised = get_normalised_depths(ceiling_dps, ceiling_mean)
+    #print 'ceiling sorted'
+    #print ' - mean length:', len(ceiling_mean)
+    #print ' - dps length: ', len(ceiling_dps)
+    #print ' - dps set len:', set([len(i) for i in ceiling_dps])
+    #print ''
     
-    #chair_pts          = get_object_points(label, 5)
-    #chair_dps          = get_depth_values(chair_pts)
+    ## chair
+    #chair_dps          = get_object_depths(img_labels, 5, 15)
     #chair_mean         = mean_depth(chair_dps)
-    #chair_normalised   = get_normalised_depth(chair_dps, chair_mean)
-    #print 'chairs sorted, length', len(chair_mean)
+    #chair_normalised   = get_normalised_depths(chair_dps, chair_mean)
+    #print 'chair sorted'
+    #print ' - mean length:', len(ceiling_mean)
+    #print ' - dps length: ', len(ceiling_dps)
+    #print ' - dps set len:', set([len(i) for i in ceiling_dps])
+    #print ''
  
+    ## construct targets
     #targets = []
     #targets.extend([0]*len(bk_mean))
     #targets.extend([4]*len(ceiling_mean))
     #targets.extend([5]*len(chair_mean))
-     
     #print 'targets, length', len(targets)
 
+    ## construct features
     #features = []
     #features.extend(bk_normalised)
     #features.extend(ceiling_normalised)
     #features.extend(chair_normalised)
-
     #print 'features, length', len(features)
+    #print ''
+
+    ## store features and targets to file
+    #store_output(create_ft_dict(features, targets), 'features_targets.p', path)
+    #print 'stored dict'
+    #print ''
+
+    # find depths of an image
+    #area_dps            = gen_area_depths(640,480,15)
+    #area_dps_mean       = mean_depth(area_dps)
+    #area_dps_normalised = get_normalised_depths(area_dps, area_dps_mean)
+    #print 'area_depths'
+    #print ' - length:', len(area_dps)
+    #print ' - set length:', set([len(i) for i in area_dps_normalised])
+    #print ''
     
-    #store_output_dict(features,targets)
-    
-    #test = gen_areas(640, 480, 15)
-    #test_dps = get_depth_values(test)
-    #test_means = mean_depth(test_dps) 
-    #test_normalised = get_normalised_depth(test_dps, test_means)
-    #print len(test_dps)
-    
-    #store_output(gen_area_depths(640,480,15), 'area_depths_6.p')
-    depths            = gen_area_depths(640,480,15)
-    depths_mean       = mean_depth(depths)
-    depths_normalised = get_normalised_depth(depths, depths_mean)
-    store_output(depths_normalised, 'area_depths_6.p')
-    #print len(output[0])
+    ## store this output 
+    #store_output(area_dps_normalised, 'area_depths_6.p', path)
+    #print 'stored area_depths'
+    #print 'done'
+
+    #print gen_area_depths(6,6,3)
--- models/model.py
+++ models/model.py
@@ -2,19 +2,51 @@
 from sklearn import svm
 from sklearn import cross_validation
 import cPickle as pickle
+import numpy as np
+import scipy as sp
 
-path = '/Users/melaus/repo/uni/individual-project/data/'
+path = '/Users/melaus/repo/uni/individual-project/data/py-data/'
 
 # load the data in, we all need it
-data = pickle.load(open(path+'py-data/features_targets.p', 'rb'))
+data = pickle.load(open(path+'features_targets.p', 'rb'))
 
 
-def modelling():
-    model = svm.SVC(kernel='linear', C=1)
-    scores = cross_validation.cross_val_score(model, data.features, data.targets, cv=2)
+"""
+run model
+"""
+def model_svc(kernel_type):
+    if kernel_type == 'linear':
+        model = svm.SVC(kernel='linear', C=1, random_state=1)
+    elif kernel_type == 'rbf':
+        model = svm.SVC(kernel='rbf', C=1, gamma=0.001, random_state=1)
+    model.fit(data['features'], data['targets'])
+
+    return model
+
+"""
+get cross val score
+"""
+def cross_val(model):
+    scores = cross_validation.cross_val_score(model, data['features'], data['targets'], cv=2)
     return scores.mean()
 
-if __name__ == '__main__':
-    data.keys()
-    #print modelling()
 
+"""
+store output to a file
+"""
+def store_output(data, filename, path=''):
+    pickle.dump(data, open(path+filename, 'wb'))
+
+
+"""
+run
+"""
+if __name__ == '__main__':
+    #store_output(model_svc('linear'), 'svc_linear_6.p', path)
+    #store_output(model_svc('rbf'), 'svc_rbf_6.p', path)
+
+    model = svm.SVC(kernel='linear', C=1, random_state=1)
+    print 'linear score:', cross_val(model)
+    
+    #model = svm.SVC(kernel='rbf', C=1, gamma=0.001, random_state=1)
+    #print 'rbf score:', cross_val(model)
