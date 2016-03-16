c6410bf95dad070fda3d6c3fe6dba77c075df9ad
Alan Lau (Balena) <awll20@bath.ac.uk>
2016-03-16 19:25:16 +0000

    removed symlink and new transform_data slurms for coordinates for simplier access

--- test/test.py
+++ test/test.py
@@ -1,7 +1,16 @@
 
-import numpy as np
-from sklearn import datasets as d
+# import numpy as np
+# from sklearn import datasets as d
 
-iris = d.load_iris()
-print 'iris feature: ', iris.data.shape
-print 'iris target:  ', iris.target.shape
+# iris = d.load_iris()
+# print 'iris feature: ', iris.data.shape
+# print 'iris target:  ', iris.target.shape
+
+import os
+import ipyparallel as ipp
+
+rc = ipp.Client()
+ar = rc[:].apply_async(os.getpid)
+pid_map = ar.get_dict()
+
+print pid_map
