10f1258a413a1f7b204d51bef77b2f8f9a24fabe
Alan Lau <melaus@me.com>
2016-03-16 19:26:32 +0000

    in the process of making it command-line friendly

--- models/model.py
+++ models/model.py
@@ -2,20 +2,22 @@
 from sklearn import svm
 from sklearn import cross_validation
 import cPickle as pickle
+import argparse
 import numpy as np
 import scipy as sp
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.grid_search import GridSearchCV
 
 
 """
-run model
+fit SVC models
 """
 def model_svc(kernel_type, features, targets):
     if kernel_type == 'linear':
         model = svm.SVC(kernel='linear', C=1, random_state=1)
     elif kernel_type == 'rbf':
         model = svm.SVC(kernel='rbf', C=1, gamma=0.001, random_state=1)
-    model.fit(train_x, train_y)
+    model.fit(features, targets)
 
     return model
 
@@ -51,10 +53,63 @@ def get_cf_matrix(actual, predicted):
 
 
 """
+GridSearch for parameters
+"""
+def grid_search(params_dict):
+    search = GridSearchCV()
+    # search.fit()
+    return score
+    pass
+
+"""
 store output to a file
 """
-def store_output(data, filename, path=''):
+def save_data(data, filename, path=''):
     pickle.dump(data, open(path+filename, 'wb'))
+
+
+"""
+command line argument parser
+"""
+def parser():
+    parser     = argparse.ArgumentParser(description='train models and some associated tasks')
+    subparsers = parser.add_subparsers(help='different arguments for different model training and associated tasks')
+
+    # svc
+    p_svc = subparsers.add_parser('svc', help='SVC')
+    p_svc.add_argument('-k', '-kernel', action='store', dest='kernel', help='choose kernel - rbf, linear')
+    p_svc.add_argument('-gamma', action='store', type=int, dest='gamma', help='dimension of a patch')
+    p_svc.set_defaults(which='svc')
+
+    # confusion matrix
+    p_cf = subparsers.add_parser('cf', help="confusion matrix")
+    p_cf.add_argument('-x', '-width', action='store', type=int, dest='x', help='width of image')
+    p_cf.add_argument('-y', '-height', action='store', type=int, dest='y', help='height of image')
+    p_svc.set_defaults(which='cf')
+
+    return parser.parse_args()
+
+
+"""
+check if there are any none arguments
+"""
+def check_args(args):
+    for key, val in vars(args).iteritems():
+        # don't check for optional keys
+        if val is None and key not in ('x', 'y'):
+            return False
+    return True
+
+
+"""
+ENTRY
+
+main function
+"""
+def main():
+    # path = '/Users/melaus/repo/uni/individual-project/data/py-data/'
+    # path = '/beegfs/scratch/user/i/awll20/data/ip/'
+    pass
 
 
 """
@@ -63,6 +118,7 @@ run
 if __name__ == '__main__':
     """ the data """ 
     path = '/Users/melaus/repo/uni/individual-project/data/py-data/'
+    path = '/beegfs/scratch/user/i/awll20/data/ip/'
     #data = pickle.load(open(path+'features_targets.p', 'rb'))
 
 
