8f77d624071434ab36ad540861f0f1a8acf9ae37
Alan Lau (Balena) <awll20@bath.ac.uk>
2016-05-05 03:19:34 +0100

    reordered arguments for precision_recall

--- models/prediction.py
+++ models/prediction.py
@@ -110,7 +110,7 @@ def prediction(model, data):
 """
 precision-recall report
 """
-def precision_recall(predicted, original):
+def precision_recall(original, predicted):
     print classification_report(original, predicted)
 
 
@@ -196,10 +196,10 @@ def main():
         print 'saved generated image'
 
     elif args.which == 'precall':
-        predicted = load_data(args.predicted, 'np', path+'prediction/').tolist()
+        predicted = load_data(args.predicted, 'np', path+'prediction/')
         original = load_data(args.original, 'np', path+'lbl/').tolist()
 
-        precision_recall(predicted, original)
+        precision_recall(original['targets'], predicted)
 
     else:
         # error message
